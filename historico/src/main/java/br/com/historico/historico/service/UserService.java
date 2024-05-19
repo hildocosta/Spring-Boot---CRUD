dfe50b88a68efd3aef5a4d02d5048e01277179e0
Hildo Pereira Costa <99414102+hildocosta@users.noreply.github.com>
2024-05-19 10:47:43 -0300

    Adicionar interface UserService para gerenciamento de usuários
    - Implementar a interface UserService na package br.com.historico.historico.service
    - Definir métodos para operações CRUD:
      - create(UserRequest userRequest): cria um novo usuário
      - getAll(Pageable pageable): recupera uma lista paginada de usuários
      - update(Long id, UserRequest userRequest): atualiza um usuário existente
      - get(Long id): recupera os dados de um usuário específico
      - delete(Long id): exclui um usuário existente

--- historico/src/main/java/br/com/historico/historico/service/UserService.java
+++ historico/src/main/java/br/com/historico/historico/service/UserService.java
@@ -1,4 +1,21 @@
 package br.com.historico.historico.service;
 
-public class UserService {
+import br.com.historico.historico.model.request.UserRequest;
+import br.com.historico.historico.model.response.UserResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface UserService {
+
+    UserResponse create(UserRequest userRequest);
+
+    Page<UserResponse> getAll(Pageable pageable);
+
+    Optional<UserResponse> update(Long id, UserRequest userRequest);
+
+    Optional<UserResponse> get(Long id);
+
+    boolean delete(Long id);
 }
