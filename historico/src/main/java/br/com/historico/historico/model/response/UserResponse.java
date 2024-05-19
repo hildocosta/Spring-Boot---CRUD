2de1878a05406c31ba9f835c85d264b250cef4b7
Hildo Pereira Costa <99414102+hildocosta@users.noreply.github.com>
2024-05-19 10:23:18 -0300

    Adicionar classe UserResponse para encapsular dados de resposta de usuários
    - Implementar a classe UserResponse na package br.com.historico.historico.model.response
    - Adicionar campos: id, name, document, login, email, password
    - Definir getters e setters para todos os campos

---
+++ historico/src/main/java/br/com/historico/historico/model/response/UserResponse.java
@@ -0,0 +1,64 @@
+package br.com.historico.historico.model.response;
+
+public class UserResponse {
+
+    private Long id;
+
+    private String name;
+
+    private String document;
+
+    private String login;
+
+    private String email;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
