43143e9a82c30874a8b8bb617851e9ebcfbfa53f
Hildo Pereira Costa <99414102+hildocosta@users.noreply.github.com>
2024-05-19 18:37:23 -0300

    Adicionar classe WeaponResponse para respostas de arma
    - Adicionados atributos: id, situacao, patrimonio, tipo, distribuicao, propriedade, observacao, numeroSerie, marca, modelo, calibre, cano, raias, acabamento
    - Implementados getters e setters para todos os atributos

---
+++ historico/src/main/java/br/com/historico/historico/model/response/WeaponResponse.java
@@ -0,0 +1,144 @@
+package br.com.historico.historico.model.response;
+
+public class WeaponResponse {
+
+    private Long id;
+
+    private String situacao;
+
+    private String patrimonio;
+
+    private String tipo;
+
+    private String distribuicao;
+
+    private String propriedade;
+
+    private String observacao;
+
+    private String numeroSerie;
+
+    private String marca;
+
+    private String modelo;
+
+    private String calibre;
+
+    private String cano;
+
+    private String raias;
+
+    private String acabamento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public String getPatrimonio() {
+        return patrimonio;
+    }
+
+    public void setPatrimonio(String patrimonio) {
+        this.patrimonio = patrimonio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDistribuicao() {
+        return distribuicao;
+    }
+
+    public void setDistribuicao(String distribuicao) {
+        this.distribuicao = distribuicao;
+    }
+
+    public String getPropriedade() {
+        return propriedade;
+    }
+
+    public void setPropriedade(String propriedade) {
+        this.propriedade = propriedade;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(String numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCalibre() {
+        return calibre;
+    }
+
+    public void setCalibre(String calibre) {
+        this.calibre = calibre;
+    }
+
+    public String getCano() {
+        return cano;
+    }
+
+    public void setCano(String cano) {
+        this.cano = cano;
+    }
+
+    public String getRaias() {
+        return raias;
+    }
+
+    public void setRaias(String raias) {
+        this.raias = raias;
+    }
+
+    public String getAcabamento() {
+        return acabamento;
+    }
+
+    public void setAcabamento(String acabamento) {
+        this.acabamento = acabamento;
+    }
+}
